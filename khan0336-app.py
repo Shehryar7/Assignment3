7eb8bad8c17ffb2c74a5ce141002fbbd24c5ece0
Shehryar Khan <khan0336@algonquinlive.com>
2023-11-23 14:04:47 -0500

    showEuro() function finished.

--- khan0336-app.py
+++ khan0336-app.py
@@ -21,12 +21,28 @@ def showUsd():
     print(time,code,rate)
 
 def showGbp():
-    #Your code will go here, remove the pass once you are done editing your code
-    pass
+    """
+    This function sends a GET request to CoinDesk API and shows Bitcoin's current price in GBP.
+    """
+    response = requests.get(url)  # sending request to url
+    jsonResponse = response.json()  # saving response as JSON
+    time = jsonResponse["time"]["updated"]  # getting time from response
+    code = jsonResponse["bpi"]["GBP"]["code"]  # getting currency code from response
+    rate = jsonResponse["bpi"]["GBP"]["rate"]  # getting price in GBP from response.
+
+    print(time, code, rate)
 
 def showEuro():
-    #Your code will go here, remove the pass once you are done editing your code
-    pass
+    """
+    This function sends a GET request to CoinDesk API and shows Bitcoin's current price in EUR.
+    """
+    response = requests.get(url)  # sending request to url
+    jsonResponse = response.json()  # saving response as JSON
+    time = jsonResponse["time"]["updated"]  # getting time from response
+    code = jsonResponse["bpi"]["EUR"]["code"]  # getting currency code from response
+    rate = jsonResponse["bpi"]["EUR"]["rate"]  # getting price in EUR from response.
+
+    print(time, code, rate)
 
 while True:
     try:
